ffa53afe7df08c8263449b9766d4c2103b2e2ed6
unknown <icaro.contato@gmail.com>
2022-06-30 14:07:16 -0300

    [Aula45] UseState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
+// Utilizando Hooks
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+  // Utilizando CallBack prevCounter
+  const handleIncrement = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>contador: {counter}</h1>
+        <button type="button" onClick={handleClick}>
+          reverse
+        </button>
+
+        <button type="button" onClick={handleIncrement}>
+          Incrementar
+        </button>
       </header>
     </div>
   );
 }
+
+// Utilizando Classes e States
+// class App extends Component {
+//   state = {
+//     reverse: true,
+//   };
+
+//   handleClick = () => {
+//     const { reverse } = this.state;
+//     this.setState({
+//       reverse: !reverse,
+//     });
+//   };
+
+//   render() {
+//     const { reverse } = this.state;
+//     const reverseClass = reverse ? 'reverse' : '';
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+//           <button type="button" onClick={this.handleClick}>
+//             Reverse
+//           </button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
