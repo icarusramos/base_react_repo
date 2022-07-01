e230a36f8f9eef08b39ecf4396a8cd1d5a404abe
unknown <icaro.contato@gmail.com>
2022-07-01 10:42:40 -0300

    [Aula48] UseEffect

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Utilizando Hooks
 function App() {
-  const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [counter2, setCounter2] = useState(0);
 
-  const handleClick = () => {
-    setReverse(!reverse);
+  const eventFn = () => {
+    console.log('h1 clicado');
   };
-  // Utilizando CallBack prevCounter
-  const handleIncrement = () => {
-    setCounter((prevCounter) => prevCounter + 1);
-  };
+
+  // //componentDidMount - Excuta apenas 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    //componentWillUnmount - Executa apenas 1x
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  // //componentDidUpdate - Excuta apenas quando o componente for atualizado
+  // useEffect(() => {
+  //   console.log('didUpdate');
+  // });
+
+  //Com dependencia - Excuta quando mudar a dependencia
+  useEffect(() => {
+    console.log('c1: ', counter, 'c2: ', counter2);
+  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>contador: {counter}</h1>
-        <button type="button" onClick={handleClick}>
-          reverse
-        </button>
-
-        <button type="button" onClick={handleIncrement}>
-          Incrementar
-        </button>
-      </header>
+      <p>teste</p>
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
+      <br />
+      <h1>Contador: {counter2}</h1>
+      <button onClick={() => setCounter2(counter2 + 2)}>Incrementar 2</button>
     </div>
   );
 }
